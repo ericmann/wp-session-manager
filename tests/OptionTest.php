c027bd04ee362a27bde7f47e037fc1627d79c098
Eric Mann <eric@eamann.com>
2018-12-27 18:29:21 -0800

    Test option validity

--- tests/OptionTest.php
+++ tests/OptionTest.php
@@ -5,8 +5,24 @@ namespace EAMann\WPSession;
 use EAMann\WPSession\Objects\Option;
 use PHPUnit\Framework\TestCase;
 
+function ini_get($setting)
+{
+    return OptionTest::ini_get($setting);
+}
+
 class OptionTest extends TestCase
 {
+    private static $ini = [];
+
+    public static function ini_get($setting)
+    {
+        if (isset(self::$ini[$setting])) {
+            return self::$ini[$setting];
+        }
+
+        return null;
+    }
+
     public function test_read_only()
     {
         $now = time();
@@ -33,5 +49,23 @@ class OptionTest extends TestCase
         }
 
         $this->assertTrue($thrown);
+    }
+
+    public function test_valid_option()
+    {
+        self::$ini['session.gc_maxlifetime'] = 500;
+
+        $option = new Option('data', time());
+
+        $this->assertTrue($option->isValid());
+    }
+
+    public function test_invalid_option()
+    {
+        $now = time();
+
+        $option = new Option('data', $now - 500);
+
+        $this->assertFalse($option->isValid(0, $now));
     }
 }
